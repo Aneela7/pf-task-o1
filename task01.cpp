8e0725d2e19b0fb83aeb7c1ddbaae038f654db5b
shakil <shakilmultan@gmail.com>
2024-10-14 21:49:52 +0500

    my first repo

---
+++ task01.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1=70;
+    int n2=50;
+    int sum =n1+n2;
+    cout<<sum<<endl;
+    int sub=n1-n2;
+    cout<<sub<<endl;
+    int mul=n1*n2;
+    cout<<mul<<endl;
+    int divid=n1/n2;
+    cout<<divid<<endl;
+}
